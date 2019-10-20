2426eb28d2827e611511471b54fc60ab9a43f475
Yennifer Marcela Tobon Yate <joshuaciencia2016@gmail.com>
2019-10-20 11:28:57 -0500

    no name fixed

--- scripts/main.gd
+++ scripts/main.gd
@@ -31,9 +31,9 @@ func create_star(d):
 	$PanelContainer/VBoxContainer/name.text += d["name"]
 	$PanelContainer/VBoxContainer/mass.text += d["mass"] + " Sun masses"
 	$PanelContainer/VBoxContainer/temperature.text += d["temperature"] + " Kelvin"
-	$PanelContainer/VBoxContainer/luminosity.text += d["luminosity"] + " Lumens"
+	$PanelContainer/VBoxContainer/luminosity.text += d["luminosity"] + " LO"
 	$PanelContainer/VBoxContainer/radius.text += d["radius"] + " Sun Radius"
-	$PanelContainer/VBoxContainer/type.text += d["type"]
+	$PanelContainer/VBoxContainer/type.text += d["type"] if d["type"] != "no name" else "white dwarfs"
 	$info2/label.text = "[Star-Name] has been borned!\nBut, it seems lonely in the massive nothingness...\nLet’s create a planet based on [Star-Name] so it’s lonely\nnever more"
 	$info2/label.text = $info2/label.text.replace("[Star-Name]", d["name"])
 	var Star = stars[d["type"]]
